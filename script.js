9b527588333b05481d068797a2daeb5911fe21ab
Cor-Ina <115652409+Cor-Ina@users.noreply.github.com>
2023-03-16 14:01:25 -0400

    Updates

--- script.js
+++ script.js
@@ -1,46 +1,29 @@
-// DOM Elements
+
+// ******************** MAIN PAGE *********************** //
 const mainPage = document.querySelector('.main-page');
 const loginPage = document.querySelector('.login-page');
-const middleContent = document.querySelector('.middle-content');
-
-const newsFeedPage = document.querySelector('.feeds-page');
-const loginModal = document.querySelector('.login-modal');
-const modalX = document.querySelector('.login-modal i');
-const loginFormBtn = document.querySelector('.login-form-btn');
-const postBtn = document.querySelector('.post-btn');
-const modalWrapper = document.querySelector('.modal-wrapper');
-const modal = document.querySelector('.modal');
-const postModalX = document.querySelector('.modal-header i');
-const modalPostBtn = document.querySelector('.modal-header button');
-const modalFooterPlus = document.querySelector('.modal-footer span');
-const modalInput = document.querySelector('.modal-input');
-const user = document.querySelector('.user');
-const sidebar = document.querySelector('.sidebar');
-const sidebarWrapper = document.querySelector('.sidebar-wrapper');
-const xBtn = document.querySelector('.sidebar-header i');
-const toggle = document.querySelector('.toggle');
-const circle = document.querySelector('.circle');
-/**************************************/
-/**************************************/
-
-// Main page
 const goToLoginPage = () => {
 	mainPage.style.display = 'none';
 	loginPage.style.display = 'grid';
 };
 
-middleContent.addEventListener('click', e => {
+const rightContent = document.querySelector('.right-content');
+rightContent.addEventListener('click', e => {
 	if (e.target.classList[1] === 'main-btn') {
 		goToLoginPage();
 	}
 });
 
 
-// Login page
+// ******************** LOGIN PAGE *********************** //
+const loginModal = document.querySelector('.login-modal');
+const modalX = document.querySelector('.login-modal i');
 modalX.addEventListener('click', () => {
 	loginModal.style.display = 'none';
 });
 
+const newsFeedPage = document.querySelector('.feeds-page');
+const loginFormBtn = document.querySelector('.login-form-btn');
 loginFormBtn.addEventListener('click', () => {
 	const loginUserInfo = document.querySelector('.login-user-info');
 	const loginPassword = document.querySelector('.login-password');
@@ -53,22 +36,27 @@ loginFormBtn.addEventListener('click', () => {
 	}
 });
 
-// News feed page
-// Post modal
+// ****************** FEEDS PAGE: POST MODAL ***********************//
+const modalWrapper = document.querySelector('.modal-wrapper');
+const modal = document.querySelector('.modal');
+const postBtn = document.querySelector('.post-btn');
 postBtn.addEventListener('click', () => {
 	modal.style.display = 'block';
 	modalWrapper.classList.add('modal-wrapper-display');
 });
 
+const modalPostBtn = document.querySelector('.modal-header button');
+const modalFooterPlus = document.querySelector('.modal-footer span');
 const changeOpacity = x => {
 	modalPostBtn.style.opacity = x;
 	modalFooterPlus.style.opacity = x;
 };
 
+const postModalX = document.querySelector('.modal-header i');
+const modalInput = document.querySelector('.modal-input');
 postModalX.addEventListener('click', () => {
 	modal.style.display = 'none';
 	modalWrapper.classList.remove('modal-wrapper-display');
-
 	if (modalInput.value !== '') {
 		modalInput.value = '';
 		changeOpacity(0.5);
@@ -87,27 +75,36 @@ modalInput.addEventListener('blur', e => {
 	}
 });
 
-// Sidebar
+// ****************** SIDEBAR ***************************//
+const user = document.querySelector('.user');
+const sidebar = document.querySelector('.sidebar');
+const sidebarWrapper = document.querySelector('.sidebar-wrapper');
 user.addEventListener('click', () => {
 	sidebar.classList.add('sidebar-display');
 	sidebarWrapper.classList.add('sidebar-wrapper-display');
 });
 
+const xBtn = document.querySelector('.sidebar-header i');
 xBtn.addEventListener('click', () => {
 	sidebar.classList.remove('sidebar-display');
 	sidebarWrapper.classList.remove('sidebar-wrapper-display');
 });
 
-// dark mode
+
+// ******************** DARK MODE *************************//
 const darkElements1 = document.querySelectorAll('.dark-mode-1');
 const darkElements2 = document.querySelectorAll('.dark-mode-2');
-const lighTexts = document.querySelectorAll('.light-text');
+const lightTexts = document.querySelectorAll('.light-text');
 const borders = document.querySelectorAll('.border');
 
+const toggle = document.querySelector('.toggle');
+const circle = document.querySelector('.circle');
+const dark = document.querySelector('#dark');
 toggle.addEventListener('click', () => {
 	circle.classList.toggle('move');
+	dark.textContent = (dark.textContent === "Light") ? "Dark" : "Light";
 	Array.from(darkElements1).map(darkEl1 => darkEl1.classList.toggle('dark-1'));
 	Array.from(darkElements2).map(darkEl2 => darkEl2.classList.toggle('dark-2'));
-	Array.from(lighTexts).map(lighText => lighText.classList.toggle('light'));
+	Array.from(lightTexts).map(lightText => lightText.classList.toggle('light'));
 	Array.from(borders).map(border => border.classList.toggle('border-color'));
 });
